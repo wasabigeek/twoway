fa541936da640557f362616aa61e268d34668d33
Nicholas Koh <wasabigeek@users.noreply.github.com>
2021-05-29 18:47:20 +0800

    Publish latest changes on CalendarEvent

--- app/models/calendar_event.rb
+++ app/models/calendar_event.rb
@@ -4,9 +4,17 @@ class CalendarEvent < ApplicationRecord
   has_many :calendar_event_snapshots
 
   def publish_latest_change
-    # last_change = calendar_event_snapshots.in_latest_order.first
-    # synced_datum = synced_event_datum || SyncedEventDatum.create!(
+    last_snapshot = calendar_event_snapshots.in_latest_order.first
+    # TODO: figure out race condition, maybe a combination of locks + unique constraints
+    synced_datum = synced_event_datum || SyncedEventDatum.create!(
+      name: last_snapshot.name,
+      starts_at: last_snapshot.starts_at,
+      ends_at: last_snapshot.ends_at
+    )
+    synced_datum.publish_changes_from(last_snapshot)
+  end
 
-    # )
+  def push_synced_data_to_source
+    Rails.logger.info('HERE')
   end
 end
--- app/models/synced_event_datum.rb
+++ app/models/synced_event_datum.rb
@@ -1,4 +1,17 @@
 class SyncedEventDatum < ApplicationRecord
   has_many :calendar_events
   has_many :calendar_sources, through: :calendar_events
+
+  def publish_changes_from(snapshot)
+    origin = snapshot.calendar_source
+    targets = CalendarSource
+      .joins(:syncs)
+      .where(syncs: origin.syncs.active)
+      .where.not(id: origin)
+
+    targets.each do |target|
+      target_event = calendar_events.find_or_initialize_by(calendar_source: target)
+      target_event.push_synced_data_to_source
+    end
+  end
 end
