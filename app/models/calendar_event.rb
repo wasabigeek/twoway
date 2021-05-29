d76e00d60eb9e3230bb0b7bfae95207119755172
Nicholas Koh <wasabigeek@users.noreply.github.com>
2021-05-29 17:12:02 +0800

    feat: Add process method to CalendarEventSnapshot

--- app/models/calendar_event.rb
+++ app/models/calendar_event.rb
@@ -1,4 +1,7 @@
 class CalendarEvent < ApplicationRecord
   belongs_to :calendar_source
-  belongs_to :synced_event_datum
+  belongs_to :synced_event_datum, optional: true
+
+  def publish_changes
+  end
 end
--- app/models/calendar_event_snapshot.rb
+++ app/models/calendar_event_snapshot.rb
@@ -1,4 +1,22 @@
 class CalendarEventSnapshot < ApplicationRecord
+  STATE_PROCESSED = 'processed'
+
   belongs_to :calendar_event, optional: true
   belongs_to :calendar_source
+
+  # TODO: async
+  after_commit :process, on: :create
+
+  def process
+    return if state == STATE_PROCESSED
+
+    event = calendar_event || CalendarEvent.create!(
+      external_id: external_id,
+      calendar_source: calendar_source,
+      snapshot_at: snapshot_at
+    )
+    event.publish_changes
+
+    update!(state: STATE_PROCESSED, calendar_event: event)
+  end
 end
