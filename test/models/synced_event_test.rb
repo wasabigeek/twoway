24d9f8b7e7e99421e15f409f17911c59c877cb1a
Nicholas Koh <wasabigeek@users.noreply.github.com>
2021-06-26 14:56:30 +0800

    Add first test for SyncedEvent

--- test/models/synced_event_test.rb
+++ test/models/synced_event_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class SyncedEventTest < ActiveSupport::TestCase
-  test "#synchronize" do
-    SyncedEvent.new.synchronize
+  test "#snapshots returns ordered list of CalendarEventSnapshots" do
+    older_event = calendar_event_snapshots(:notion_event_one_snapshot_one)
+    newer_event = calendar_event_snapshots(:gcal_event_one_snapshot_one)
+    newer_event.update!(snapshot_at: older_event.snapshot_at + 1.second)
+
+    synced_event = SyncedEvent.create!(
+      sync: syncs(:one),
+      calendar_events: [calendar_events(:notion_event_one), calendar_events(:gcal_event_one)]
+    )
+
+    assert synced_event.snapshots.to_a == [
+      newer_event,
+      older_event
+    ]
   end
 end
