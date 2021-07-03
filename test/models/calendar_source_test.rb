592a636c325ea1a95a2fd6132ab185755580a26d
Nicholas Koh <wasabigeek@users.noreply.github.com>
2021-07-03 21:13:11 +0800

    Start on some CalendarSource tests

---
+++ test/models/calendar_source_test.rb
@@ -0,0 +1,40 @@
+require "test_helper"
+
+class CalendarSourceCheckForNewEventsTest < ActiveSupport::TestCase
+  test "creates CalendarEvents for Google" do
+    calendar_source = calendar_sources(:gcal_one)
+
+    assert_difference -> { CalendarEvent.count }, 3 do
+      VCR.use_cassette('clients/google/list_events') do
+        calendar_source.check_for_new_events
+      end
+    end
+  end
+
+  test "creates all-day recurring exception correctly" do
+    calendar_source = calendar_sources(:gcal_one)
+    VCR.use_cassette('clients/google/list_events') do
+      calendar_source.check_for_new_events
+    end
+
+    event = CalendarEvent.find_by(external_id: '46avomjj8qkagc38r7asgvqdn2_20210620')
+    assert_not_nil(event)
+  end
+
+  # test "creates recurring event exception correctly"
+  # test "creates recurring event correctly"
+
+  test "ignores existing events" do
+    calendar_source = calendar_sources(:gcal_one)
+    event = CalendarEvent.create!(
+      calendar_source: calendar_sources(:gcal_one),
+      external_id: '46avomjj8qkagc38r7asgvqdn2_20210620'
+    )
+
+    assert_no_difference -> { CalendarEvent.where(external_id: '46avomjj8qkagc38r7asgvqdn2_20210620').count } do
+      VCR.use_cassette('clients/google/list_events') do
+        calendar_source.check_for_new_events
+      end
+    end
+  end
+end
