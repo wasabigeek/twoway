c42cdac3328d5c7d7f56690088fc53ae2e556541
Nicholas Koh <wasabigeek@users.noreply.github.com>
2021-07-11 15:57:44 +0800

    Add synced event test

--- test/models/synced_event_test.rb
+++ test/models/synced_event_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
-class SyncedEventTest < ActiveSupport::TestCase
-  test "#snapshots returns ordered list of CalendarEventSnapshots" do
+class SyncedEventSnapshotTest < ActiveSupport::TestCase
+  test "returns ordered list of CalendarEventSnapshots" do
     older_event = calendar_event_snapshots(:notion_event_one_snapshot_one)
     newer_event = calendar_event_snapshots(:gcal_event_one_snapshot_one)
     newer_event.update!(snapshot_at: older_event.snapshot_at + 1.second)
@@ -15,5 +15,28 @@ class SyncedEventTest < ActiveSupport::TestCase
       newer_event,
       older_event
     ]
+  end
+end
+
+class SyncedEventSynchronizeTest < ActiveSupport::TestCase
+  test "syncs all-day event in Notion with Google" do
+    all_day_notion_event = CalendarEvent.create!(
+      external_id: 'c25c4426-1de5-4da5-ab05-a8b59d3c0b84',
+      calendar_source: calendar_sources(:notion_cal_one)
+    )
+
+    synced_event = SyncedEvent.create!(
+      sync: syncs(:one),
+      calendar_events: [all_day_notion_event]
+    )
+
+    VCR.use_cassette('synced_event/sync_all_day_notion_event') do
+      synced_event.synchronize
+    end
+
+    assert_equal(
+      synced_event.calendar_events.reload.map { |e| e.calendar_source.connection.provider },
+      ["notion", "google_oauth2"]
+    )
   end
 end
